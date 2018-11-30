f67bccdf1fc21ece01e67138a96312f05c77fdbc
DannyBoyNYC <daniel.deverell@gmail.com>
2018-11-30 12:40:33 -0500

    initial commit

---
+++ react-pirates/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../assets/css/Header.css'
+import logo from '../assets/img/anchor.svg';
+
+const Header = (props) => {
+  return (
+    <div className="header">
+    <img src={logo} className="logo" alt="logo" />
+      <h1>{props.headline}</h1>
+    </div>)
+  }
+  
+export default Header;
---
+++ react-pirates/src/components/Pirate.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import '../assets/css/Pirate.css'
+
+class Pirate extends Component {
+  render(){
+    const { details } = this.props;
+    return (
+      <div className='pirate'>
+      <ul>
+      <li>{details.name}</li>
+      <li>{details.weapon}</li>
+      <li>{details.vessel}</li>
+      <li><button onClick={() => this.props.removePirate(this.props.index)}>✖︎</button></li>
+      {/* <li><button onClick={() => this.props.removePirate(this.props.key)}>✖︎</button></li> */}
+        </ul>
+      </div>
+      )
+    }
+  }
+  export default Pirate;
