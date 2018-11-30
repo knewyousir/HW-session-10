b9738ff985c17df07e6d1622a7889b3c09248e6a
DannyBoyNYC <daniel.deverell@gmail.com>
2018-11-30 14:41:36 -0500

    done

---
+++ src/components/App.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import Pirate from './Pirate'
+import Header from './Header'
+import PirateForm from './PirateForm'
+
+import axios from 'axios';
+
+class App extends Component {
+  
+  constructor() {
+    super();
+    this.addPirate = this.addPirate.bind(this);
+    this.removePirate = this.removePirate.bind(this);
+    this.state = {
+      pirates: {},
+      isLoading: true,
+      error: null
+    }
+  }
+  
+  componentDidMount(){
+    this.setState({ isLoading: true });
+    axios.get('http://localhost:3005/api/pirates')
+    .then(response => this.setState({
+      pirates: response.data,
+      isLoading: false
+    }))
+    .catch(error => this.setState({
+      error,
+      isLoading: false
+    }));
+  }
+  
+  render() {
+    
+    const { isLoading, error } = this.state;
+    
+    if (error) {
+      return <p>{error.message}</p>;
+    }
+    
+    if (isLoading) {
+      return <p>Loading ...</p>;
+    }
+    
+    return (
+      <div className="App">
+      <Header headline="Pirates!" />
+      
+      {
+        Object.keys(this.state.pirates)
+        .map(key =>
+          <Pirate key={key}
+          index={key}
+          details={this.state.pirates[key]}
+          removePirate={this.removePirate} />)
+        }
+        
+        <PirateForm loadSamples={this.loadSamples} addPirate={this.addPirate} />
+        </div>
+        );
+      }
+      
+      removePirate(key){
+        const pirates = { ...this.state.pirates }
+        let pirateDel = this.state.pirates[key]._id
+        axios.get(`http://localhost:3005/api/pirates/${pirateDel}`)
+        .then(delete pirates[key])
+        .then(this.setState({pirates}))
+      }
+      
+      addPirate(pirate) {
+        const localPirates = { ...this.state.pirates }
+        // console.log(localPirates)
+        axios.post('http://localhost:3005/api/pirates/', pirate)
+          .then(response => localPirates.key[pirate] = response.data)
+          // .then(Object.keys(localPirates).push(response.data))
+          .then(console.log(localPirates))
+          // .then(response => this.setState({ pirates: response.data }) // replaces all state with returned values
+          }
+      
+    }
+    
+    export default App;
+    
